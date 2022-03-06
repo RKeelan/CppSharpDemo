1fbe06839e882ad3a78e0260743949b8b801fa03
RKeelan <RKeelan@users.noreply.github.com>
2022-03-06 11:08:33 -0500

    Add CppOp.h

---
+++ CppLib/CppOp.h
@@ -0,0 +1,27 @@
+#include "CppData.h"
+
+enum class Operation {
+    Reset,
+    Increment
+};
+
+enum class ScopeFlags {
+    Public = (1 << 0),
+    Protected = (1 << 1),
+    Private = (1 << 2),
+    Bool = (1 << 3),
+    Int = (1 << 4),
+    Uint = (1 << 5),
+    Float = (1 << 6),
+    Char = (1 << 7),
+}
+
+inline ScopeFlags operator|(ScopeFlags a, ScopeFlags b)
+{
+    return static_cast<ScopeFlags>(static_cast<uint>(a) | static_cast<uint>(b));
+}
+
+class CppOp {
+public:
+    static Do(CppData data, Operation op, ScopeFlags scope);
+};
