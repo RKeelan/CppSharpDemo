039c9d49dd84b43eb348a9dfee56225d668d40fd
RKeelan <RKeelan@users.noreply.github.com>
2022-03-06 10:44:07 -0500

    Add CppLib and CppData with public integral types

---
+++ CppLib/CppData.h
@@ -0,0 +1,19 @@
+class CppData {
+public:
+    CppData() {}
+
+    bool pub_bool;
+    char pub_char;
+
+    int8_t pub_i8;
+    uint8_t pub_u8;
+    int16_t pub_i16;
+    uint16_t pub_u16;
+    int32_t pub_i32;
+    uint32_t pub_u32;
+    int64_t pub_i64;
+    uint64_t pub_u64;
+
+    float pub_f32;
+    float pub_f64;
+}
